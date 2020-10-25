18a0fe393459a051db864dd13b91f733708a7030
August <hello@aughost.codes>
2020-10-24 21:29:35 -0400

    Free square

--- main.js
+++ main.js
@@ -2,12 +2,19 @@
 
 const cardGrid = document.querySelector('.card-grid');
 
-(function create() {
+(function createGrid() {
     for (let i = 0; i < 25; i++) {
         let createdSquare = document.createElement('div');
         createdSquare.classList.add('square');
         cardGrid.appendChild(createdSquare);
     }
 })();
+
+function toggleFreeSquare() {
+    if (cardGrid.childElementCount === 25) {
+        cardGrid.childNodes[13].classList.add('free-square');
+        cardGrid.childNodes[13].textContent = 'FREE';
+    }
+}
 
 console.log('testing');
