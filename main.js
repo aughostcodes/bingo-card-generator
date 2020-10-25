5fea90dbb57f613912af6f3fdd93a16267763362
August <hello@aughost.codes>
2020-10-24 20:47:24 -0400

    Lay down

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('connected');
+'use strict';
+
+const cardGrid = document.querySelector('.card-grid');
+
+(function create() {
+    for (let i = 0; i < 25; i++) {
+        let createdSquare = document.createElement('div');
+        createdSquare.classList.add('square');
+        cardGrid.appendChild(createdSquare);
+    }
+})();
